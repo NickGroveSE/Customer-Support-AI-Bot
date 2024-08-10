3961f01053ecde1e5bb51e840b95ff131a841d55
Nick Grove <nickgrove.se@gmail.com>
2024-08-09 23:02:57 -0400

    Eye Animations

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,24 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react"
 
 export default function Home() {
+
+  const [typing, setTyping] = useState(false)
+
   return (
-    <div id="content">
-      <div>
-
+    <div id="wrapper" onClick={function(e){ e.target.tagName == 'TEXTAREA' ? setTyping(true) : setTyping(false)}}>
+      <div id="content">
+        <img className={"glow eye"} id="left-eye" src="eye.svg"/>
+        <img className={typing ? "pupil" : "pupil animated"} id="left-pupil" src="pupil.svg" style={{ transform: typing ? 'translate(13px, 20px)' : 'translate(0, 0)'}}/>
+        <img className={"glow eye"} id="right-eye" src="eye.svg"/>
+        <img className={typing ? "pupil" : "pupil animated"} id="right-pupil" src="pupil.svg" style={{transform: typing ? 'translate(13px, 20px)' : 'translate(0, 0)'}}/>
+        <form>
+          <textarea id="chat" placeholder="How Can Help?" ></textarea>
+        </form>
       </div>
-      <img id="eye-icon" src="eye.svg"/>
-      <img src="pupil.svg"/>
-      <img src="eye.svg"/>
-      <img src="pupil.svg"/>
-
     </div>
   );
 }
