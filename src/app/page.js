aca94aedb52101d7f511894adb84b00cd07c8f14
Nick Grove <nickgrove.se@gmail.com>
2024-08-09 23:32:12 -0400

    Solid UI Base Onto AI Research

--- src/app/page.js
+++ src/app/page.js
@@ -12,11 +12,11 @@ export default function Home() {
     <div id="wrapper" onClick={function(e){ e.target.tagName == 'TEXTAREA' ? setTyping(true) : setTyping(false)}}>
       <div id="content">
         <img className={"glow eye"} id="left-eye" src="eye.svg"/>
-        <img className={typing ? "pupil" : "pupil animated"} id="left-pupil" src="pupil.svg" style={{ transform: typing ? 'translate(13px, 20px)' : 'translate(0, 0)'}}/>
+        <img className={typing ? "pupil focused" : "pupil idle"} id="left-pupil" src="pupil.svg"/>
         <img className={"glow eye"} id="right-eye" src="eye.svg"/>
-        <img className={typing ? "pupil" : "pupil animated"} id="right-pupil" src="pupil.svg" style={{transform: typing ? 'translate(13px, 20px)' : 'translate(0, 0)'}}/>
+        <img className={typing ? "pupil focused" : "pupil idle"} id="right-pupil" src="pupil.svg"/>
         <form>
-          <textarea id="chat" placeholder="How Can Help?" ></textarea>
+          <textarea id="chat" placeholder="How can I help?" ></textarea>
         </form>
       </div>
     </div>
